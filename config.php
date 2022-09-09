500b34bfef09cdd1e0fcd72747dc36c3ff23ec93
Damian Ligmann <dligmann@MacBook-Pro-dligmann.local>
2022-09-09 11:18:17 +0200

    add db

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
-$DB = mysqli_connect(
-	"localhost:33065",
-	"root",
-	"root",
-	"libery"
-	);
-?>
+
+$config = require_once 'database.php';
+
+try {
+	$db = new PDO("mysql:host={$config['host']};dbname={$config['database']};charset=utf8", $config['user'], $config['password'], [
+		PDO::ATTR_EMULATE_PREPARES => false,
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION //wyświetla błędy
+	]);
+} catch (PDOException $error) {
+		exit('Database error');
+}
