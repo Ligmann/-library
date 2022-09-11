009e83ec3d4fb3a5cc83272483f24181c64d4507
Damian Ligmann <dligmann@MacBook-Pro-dligmann.local>
2022-09-11 21:35:40 +0200

    error styles

--- login.php
+++ login.php
@@ -8,16 +8,14 @@ $user = $statement->fetch(PDO::FETCH_OBJ);
 if ($user !== false && $user->password === $_POST['password']) {
 	$_SESSION['user'] = $user;
 	header('location: /');
-} else {
-	$error = "błąd nazwy lub hasła";
-}
+};
 ?>
 <main class="login d-flex align-items-center">
 	<div class="container d-flex justify-content-center">
 		<div class="login__container px-4 py-3">
 			<div class="d-flex align-items-center mb-4">
 				<img class="login__images" src="images/logo.png" alt="">
-				<h2 class="ps-3 header-text">Logowanie do<br /> panelu biblioteki</h2>
+				<h2 class="ps-3 header-text">Biblioteczka <br /> Domowego kościoła</h2>
 			</div>
 			<form class="login__form d-flex flex-column" action="" method="post">
 				<span class="d-flex">
@@ -36,9 +34,11 @@ if ($user !== false && $user->password === $_POST['password']) {
 					</span>
 					<input name="password" type="password" class="input input--max-width mt-3" placeholder="Podaj hasło" required>
 				</span>
-				<span>
-				<?= $error ?>
-				</span>
+				<?php if($error): ?>
+					<span class="input__error mt-2 ms-5">
+						<?= $error ?>
+					</span>
+				<?php endif ;?>
 				<input class="button--primary mt-4" type="submit" value="Zaloguj się">
 			</form>
 		</div>
