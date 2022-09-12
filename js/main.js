c3b789c80d9e9a1c5e7dffaa75c35b0a0e0d4957
Damian Ligmann <dligmann@MacBook-Pro-dligmann.local>
2022-09-12 12:42:52 +0200

    fix nav js

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ let buttonModalBook = document.querySelectorAll(".js-button-read-more");
 
 buttonModalBook.forEach(e => {
 	let idButton = e.dataset.id;
-	e.addEventListener("click", ()=> {
+	e.addEventListener("click", () => {
 		let modalBook = document.querySelectorAll(".js-book-modal");
 		let exitModal = document.querySelectorAll(".js-modal-exit");
 
 		modalBook.forEach(modal => {
 			let idModal = modal.dataset.id_modal;
-			if(idButton === idModal) {
+			if (idButton === idModal) {
 				modal.classList.add('js-modal-active');
 			};
 
 			exitModal.forEach(exitIteral => {
-				exitIteral.addEventListener("click", ()=> {
+				exitIteral.addEventListener("click", () => {
 					modal.classList.remove('js-modal-active');
 				});
 			});
@@ -22,38 +22,41 @@ buttonModalBook.forEach(e => {
 	});
 });
 
-//	NAV
-let buttonNav = document.querySelector(".js-button-nav");
-let nav = document.querySelector(".js-nav");
-let bodyNavStyles = document.querySelector("body");
-
-buttonNav.addEventListener("click", e=> {
-	nav.classList.add("js-nav-active");
-	bodyNavStyles.classList.add("js-nav-hidden");
-});
-
-let exitNav = document.querySelector(".js-nav-exit");
-exitNav.addEventListener("click", e=> {
-	nav.classList.remove("js-nav-active");
-	bodyNavStyles.classList.remove("js-nav-hidden");
-});
-
 //	LOGOUT
 let logout = document.querySelectorAll(".js-logout");
 
 logout.forEach(e => {
-	e.addEventListener("click", ()=> {
+	e.addEventListener("click", () => {
 		var url = 'http://dev.library.pl/logout.php';
 		var formData = new FormData();
 		formData.append('post_button', true);
 
 		fetch(url, { method: 'POST', credentials: 'include', body: formData })
-		.then(function (response) {
-			return response.text();
-		})
-		.then(function (body) {
-			// console.log(body);
-			window.location.reload();
-		});
+			.then(function (response) {
+				return response.text();
+			})
+			.then(function (body) {
+				// console.log(body);
+				window.location.reload();
+			});
 	});
+});
+
+//	NAV
+function navAction() {
+	let nav = document.querySelector(".js-nav");
+	let bodyNavStyles = document.querySelector("body");
+
+	nav.classList.toggle("js-nav-active");
+	bodyNavStyles.classList.toggle("js-nav-hidden");
+};
+
+let buttonNav = document.querySelector(".js-button-nav");
+buttonNav.addEventListener("click", e => {
+	navAction();
+});
+
+let exitNav = document.querySelector(".js-nav-exit");
+exitNav.addEventListener("click", e => {
+	navAction();
 });
